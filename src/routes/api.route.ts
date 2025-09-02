82bc7d3899cd67d46bb222a7f4562b2755fa0f8a
Alireza Aberoumand <maya7192@yahoo.com>
2025-09-02 11:00:12 -0700

    Refactor API routes: replace users route with user route, add admin, song, and album routes, and remove unused users.route file.

--- src/routes/api.route.ts
+++ src/routes/api.route.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
-import usersRoute from './users.route.js';
+import userRoute from './user.route.js';
 import authRoute from './auth.route.js';
+import adminRoute from './admin.route.js';
+import songRoute from './song.route.js';
+import albumRoute from './album.route.js';
 
 const router = Router();
 
-router.use('/users', usersRoute);
+router.use('/user', userRoute);
 router.use('/auth', authRoute);
+router.use('/admin', adminRoute);
+router.use('/song', songRoute);
+router.use('/album', albumRoute);
 
 export default router;
