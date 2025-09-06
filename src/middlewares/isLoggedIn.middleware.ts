c489b667b7735a979fffe0cb2e9bd6f5b88ac4a8
Alireza Aberoumand <maya7192@yahoo.com>
2025-09-06 12:06:38 -0700

    Integrate Clerk authentication middleware into the app, update package dependencies to include @clerk/express, and remove the obsolete bad JSON error handler utility.

---
+++ src/middlewares/isLoggedIn.middleware.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from 'express';
+
+const isLoggedIn = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.auth.userId) {
+    return res
+      .status(401)
+      .json({
+        message: 'Unauthorized - You must be logged in to access this resource',
+        success: false,
+      });
+  }
+  next();
+};
+
+export { isLoggedIn };
