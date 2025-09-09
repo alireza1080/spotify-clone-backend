568c5c8873ceea559f7c77b0ac9f9ce0a0047c32
Alireza Aberoumand <maya7192@yahoo.com>
2025-09-09 09:50:03 -0700

    Add express-fileupload dependency and configure file upload settings in app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,28 @@ import helmet from 'helmet';
 import apiRoute from './routes/api.route.js';
 import { badJsonErrorHandler } from './middlewares/badJsonErrorHandler.middleware.js';
 import { clerkMiddleware } from '@clerk/express';
+import fileUpload from 'express-fileupload';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: './temp',
+    createParentPath: true,
+    limits: {
+      fileSize: 1024 * 1024 * 5,
+    },
+    abortOnLimit: true,
+    responseOnLimit: 'File too large',
+    debug: true,
+    safeFileNames: true,
+    preserveExtension: true,
+    uploadTimeout: 10000,
+  })
+);
 
 app.use(clerkMiddleware());
 
