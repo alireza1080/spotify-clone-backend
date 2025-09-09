d0c30dae21595253c4555107c0d024971610f4ee
Alireza Aberoumand <maya7192@yahoo.com>
2025-09-09 09:39:50 -0700

    Add cloudinary dependency and update admin route to handle song creation

---
+++ src/utils/cloudinary.ts
@@ -0,0 +1,13 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { config } from 'dotenv';
+
+config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME as string,
+    api_key: process.env.CLOUDINARY_API_KEY as string,
+    api_secret: process.env.CLOUDINARY_API_SECRET as string,
+    api_folder: process.env.CLOUDINARY_API_FOLDER as string,
+});
+
+export default cloudinary;
